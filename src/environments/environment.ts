9a062107d04c48ec183201e42046f9122ac04c69
Harsh Hajela <harsh.hajela@gmail.com>
2025-09-10 11:51:55 +0100

    Ignore Angular environment files; add example template

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    spaceId: '115cvm9e10qe',
-    accessToken: 'bOmDAlbNZ_gkXy4vRuJmNb00BLYSvIjUN-Ux0vZOid4',
-    previewKey: '8wozT5n4hQCnb3X35763fVL2A3_gBNcOq7CMfafjfEA'
-};
