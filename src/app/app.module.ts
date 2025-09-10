49eca9c401004bc06ff9728d6b75574cf7472844
Harsh Hajela <harsh.hajela@gmail.com>
2025-09-10 11:29:17 +0100

    UI fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { ToHtmlPipe } from './to-html.pipe';
+import { HeaderComponent } from './header/header.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogPostComponent,
+    ToHtmlPipe,
+    HeaderComponent,
+    BlogsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
