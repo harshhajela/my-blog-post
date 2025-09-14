202faa88981cc12f1c9c558fb443388214d31b2f
Harsh Hajela <60396360+harshhajela@users.noreply.github.com>
2025-09-14 22:26:28 +0100

    Create environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  spaceId: '115cvm9e10qe',
+  accessToken: 'bOmDAlbNZ_gkXy4vRuJmNb00BLYSvIjUN-Ux0vZOid4',
+  previewKey: '8wozT5n4hQCnb3X35763fVL2A3_gBNcOq7CMfafjfEA'
+};
